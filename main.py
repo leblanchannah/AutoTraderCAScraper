25b14d2e2f90eeb65b9fc194eb6497faf5658e7d
Hannah LeBlanc <leblanchannah95@gmail.com>
2024-10-21 02:24:17 +0000

    including id in data - assuming this is unique for listings, still need to check for dupes, search results for rav4 and forester

--- main.py
+++ main.py
@@ -26,6 +26,13 @@ def clean_text(dom_element):
 
 
 def parse_search_result(soup):
+    try:
+        item_id = soup.get('id')
+    except KeyError as e:
+        print("Unable to get id from")
+        print(e)
+        item_id = None
+
     title = soup.find('span', class_='title-with-trim')
     title = clean_text(title)
     title_details = split_title(title)
@@ -38,6 +45,7 @@ def parse_search_result(soup):
     url = soup.find("a", class_="detail-price-area").get("href")
 
     search_entry = {
+        'id':item_id,
         'title':title,
         'proximity':clean_text(proximity),
         'odometer':clean_text(odometer),
@@ -49,10 +57,9 @@ def parse_search_result(soup):
     }
     return {**search_entry, **title_details}
 
-def search_sales(make, model, city, province, postal_code, n_records=100, page=0):
-
-    url = f"https://www.autotrader.ca/cars/{make}/{model}/{province}/{city}/?rcp={n_records}&rcs={page}&srt=35&prx=100&prv=Ontario&loc={postal_code}&hprc=True&wcp=True&sts=New-Used&inMarket=basicSearch"
-    print(url)
+def get_search(make, model, province, postal_code, n_records=100, page=0):
+# https://www.autotrader.ca/cars/honda/cr-v/on/?rcp=15&rcs=0&srt=35&prx=-2&prv=Ontario&loc=K1Y%201S3&hprc=True&wcp=True&sts=New-Used&adtype=Dealer&inMarket=advancedSearch
+    url = f"https://www.autotrader.ca/cars/{make}/{model}/{province}/?rcp={n_records}&rcs={page}&srt=35&prx=100&prv=Ontario&loc={postal_code}&hprc=True&wcp=True&sts=New-Used&adtype=Dealer&inMarket=advancedSearch"
     payload = {}
     headers = {
     'accept-language': 'en-US,en;q=0.9',
@@ -66,58 +73,58 @@ def search_sales(make, model, city, province, postal_code, n_records=100, page=0
     
     return soup
 
-def main():
+def advanced_search(make, model, province, postal_code, max_results):
     search_results = []
     
-    soup = search_sales('honda', 'cr-v', 'toronto','on', postal_code='M5W%201E6', n_records=100, page=0)
-    search_results.append(soup)
-    # https://www.autotrader.ca/cars/honda/cr-v/on/ottawa/?rcp=15&rcs=0&srt=35&prx=100&prv=Ontario&loc=K1Y%201S3&hprc=True&wcp=True&sts=New-Used&adtype=Dealer&inMarket=advancedSearch
-    with open("crv_search_p0.html", "w") as file:
-            file.write(str(soup))
-
+    soup = get_search(make, model, province, postal_code=postal_code, n_records=100, page=0)
+    search_results.extend(soup.find_all('div', class_='result-item'))
 
     num_results = soup.find('span', id='titleCount').text
     num_results = int(num_results.replace(',', ''))
-    # pages = len(soup.find_all('li', class_='page-item'))
-    # print(f"pages:{pages}")
+    print(f'Num results in search {num_results}')
+
     viewed = 100
-    while viewed <= num_results:
+    requested_results = max_results
+    while viewed <= num_results and viewed <= requested_results:
         page = int(viewed/100)
-        soup = search_sales('honda', 'cr-v', 'toronto','on', postal_code='M5W%201E6', n_records=100, page=page)
-        search_results.append(soup)
-        with open(f"crv_search_p{viewed}.html", "w") as file:
-            file.write(str(soup))
+        soup = get_search(make, model, province, postal_code=postal_code, n_records=100, page=page)
+        search_results.extend(soup.find_all('div', class_='result-item'))
         viewed+=100
 
+    print(f'found {len(search_results)} results')
 
-    print(len(search_results))
-
-    # with open("crv_search.html") as fp:
-    #     soup = BeautifulSoup(fp, 'html.parser')
     data = []
-    for soup in search_results:
-        print('new_page')
-        result_items = soup.find_all('div', class_='result-item')
-
-        for item in result_items:
-            search_result = parse_search_result(item)
-            data.append(search_result)
+    for item in search_results:
+        search_result = parse_search_result(item)
+        data.append(search_result)
 
     df = pd.DataFrame(data)
-    print(df.head())
-    print(df.columns)
 
-    # df['year'] = df['year'].astype(int)
     df['odometer'] = df['odometer'].str.replace("[^0-9]","",regex=True).astype(float)
     df['price'] = df['price'].str.replace("[^0-9]","",regex=True).astype(float)
 
-    df.to_csv('search_test.csv')
+    return df
 
 
+def main():
+    # postal_code = 'M5W%201E6'
+    # province = 'on'
+    # max_results = 200
 
-df = pd.read_csv('search_test.csv')
+    # df_crv = advanced_search('honda', 'cr-v', province, postal_code, 200)
+    # df_rav4 = advanced_search('toyota', 'rav4', province, postal_code, 200)
+    # df_forester = advanced_search('subaru', 'forester', province, postal_code, 200)
 
-fig = px.scatter(df, y="price", x="odometer", color="year")
-fig.update_traces(marker_size=10)
-fig.update_layout(scattermode="group", scattergap=0.75)
-fig.show()
+    # df = pd.concat([df_crv, df_rav4, df_forester])
+    # df.to_csv('search_test.csv')
+
+    df = pd.read_csv('search_test.csv')
+
+    fig = px.scatter(df, y="price", x="odometer", color="model", trendline="ols")
+    fig.update_traces(marker_size=10)
+    fig.update_layout(scattermode="group", scattergap=0.75)
+    fig.show()
+
+
+main()
+
