c8331edbe464e1e8e09d7d2ad50d4942b9ac7edd
Hannah LeBlanc <leblanchannah95@gmail.com>
2024-10-21 03:18:30 +0000

    updated plot to facet cols, prints out OLS summaries

--- main.py
+++ main.py
@@ -2,22 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd 
 import plotly.express as px
-
+import statsmodels.api as sm
 
 
 def split_title(title):
-    data = {
-        'year':"",
-        'make':"",
-        'model':"",
-        'trim':"",
-    }
     if title:
         title = title.split(" ")
-        data['year'] = title[0]
-        data['make'] = title[1]
-        data['model'] = title[2]
-    return data
+        data = {
+            'year':title[0],
+            'make':title[1],
+            'model':title[2],
+        }
+        return data
 
     
 def clean_text(dom_element):
@@ -40,7 +36,7 @@ def parse_search_result(soup):
     details = soup.find('p', class_='details used')
     odometer = soup.find('span', class_='odometer-proximity')
     price = soup.find('span', class_='price-amount')
-    price_delta = soup.find('div', class_='price-delta')
+    price_delta = soup.find('div', class_='price-delta-text')
     dealer = soup.find('div', class_='dealer-inner-area')
     url = soup.find("a", class_="detail-price-area").get("href")
 
@@ -58,7 +54,6 @@ def parse_search_result(soup):
     return {**search_entry, **title_details}
 
 def get_search(make, model, province, postal_code, n_records=100, page=0):
-# https://www.autotrader.ca/cars/honda/cr-v/on/?rcp=15&rcs=0&srt=35&prx=-2&prv=Ontario&loc=K1Y%201S3&hprc=True&wcp=True&sts=New-Used&adtype=Dealer&inMarket=advancedSearch
     url = f"https://www.autotrader.ca/cars/{make}/{model}/{province}/?rcp={n_records}&rcs={page}&srt=35&prx=100&prv=Ontario&loc={postal_code}&hprc=True&wcp=True&sts=New-Used&adtype=Dealer&inMarket=advancedSearch"
     payload = {}
     headers = {
@@ -73,19 +68,20 @@ def get_search(make, model, province, postal_code, n_records=100, page=0):
     
     return soup
 
+
 def advanced_search(make, model, province, postal_code, max_results):
     search_results = []
-    
+    # send first request to get total number of available listings - used for sending requests by page
     soup = get_search(make, model, province, postal_code=postal_code, n_records=100, page=0)
-    search_results.extend(soup.find_all('div', class_='result-item'))
+    soup_result_items = soup.find_all('div', class_='result-item')
+    search_results.extend(soup_result_items)
 
     num_results = soup.find('span', id='titleCount').text
     num_results = int(num_results.replace(',', ''))
     print(f'Num results in search {num_results}')
 
-    viewed = 100
-    requested_results = max_results
-    while viewed <= num_results and viewed <= requested_results:
+    viewed = len(soup)
+    while viewed <= num_results and viewed <= max_results:
         page = int(viewed/100)
         soup = get_search(make, model, province, postal_code=postal_code, n_records=100, page=page)
         search_results.extend(soup.find_all('div', class_='result-item'))
@@ -106,10 +102,10 @@ def advanced_search(make, model, province, postal_code, max_results):
     return df
 
 
-def main():
+if __name__ == "__main__":
     # postal_code = 'M5W%201E6'
     # province = 'on'
-    # max_results = 200
+    # max_results = 1000
 
     # df_crv = advanced_search('honda', 'cr-v', province, postal_code, 200)
     # df_rav4 = advanced_search('toyota', 'rav4', province, postal_code, 200)
@@ -120,11 +116,29 @@ def main():
 
     df = pd.read_csv('search_test.csv')
 
-    fig = px.scatter(df, y="price", x="odometer", color="model", trendline="ols")
+    df = df.dropna(subset=['odometer','year','price'], axis=0, how='any')
+
+    for model, data in df.groupby("model"):
+        print(model)
+
+        X = data[['odometer', 'year']]  # Independent variables
+        X = sm.add_constant(X)        # Adds a constant term to the predictor
+        y = data['price']  
+
+        model = sm.OLS(y, X).fit()
+        print(model.summary())
+
+    fig = px.scatter(df, y="price", x="odometer", color="year", facet_col="model", trendline='ols', trendline_color_override="black")
     fig.update_traces(marker_size=10)
-    fig.update_layout(scattermode="group", scattergap=0.75)
+    fig.update_layout(
+        title='AutoTraderCa search results on 20/10/2024',
+        yaxis_title="Price (CAD)"    
+    )
+    fig.update_xaxes(title="Mileage (km)")
+    
     fig.show()
+    fig.write_image("model_comparison_mileage_price.png")
 
 
-main()
+
 
